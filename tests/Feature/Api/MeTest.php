fe07b1da756ffafa923ad37e4d9933d30d71dad8
ryan <ryan@e-monsite.com>
2018-11-08 12:00:53 +0000

    User(Me) model related tests add

---
+++ tests/Feature/Api/MeTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+/**
+ * Class MeTest
+ *
+ * 'api/me/...' related routes tests
+ *
+ * @package Tests\Feature\Api
+ */
+class MeTest extends ApiCase
+{
+	/**
+	 * Test route.
+	 *
+	 * @group developer
+	 * @group support
+	 * @group end-user
+	 * @return void
+	 */
+	public function testRouteIndex()
+	{
+		$response = $this->get($this->getRouteUrl('me.index'), $this->getAuthorizationHeaders());
+		
+		$response->assertHeader('content-type', 'application/json');
+		$response->assertStatus(200);
+		
+		$response->assertJsonStructure([
+			'data' => [
+				'id',
+				'created_at',
+				'updated_at'
+			]
+		]);
+		
+		$response->assertJson([
+			'data' => [
+				'user_group_id'     => $this->user->user_group_id,
+				'name'              => $this->user->name,
+				'email'             => $this->user->email,
+			]
+		]);
+	}
+	
+	/**
+	 * Test route.
+	 *
+	 * @group developer
+	 * @group support
+	 * @group end-user
+	 * @return void
+	 */
+	public function testRouteNotificationIndex()
+	{
+		$response = $this->get($this->getRouteUrl('meNotification.index'), $this->getAuthorizationHeaders());
+		
+		$response->assertHeader('content-type', 'application/json');
+		$response->assertStatus(200);
+		
+		$response->assertJsonStructure([
+			'data'
+		]);
+	}
+}
